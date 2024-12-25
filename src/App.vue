fe9e03a212925733419abaabc2521278cb81e8fe
Patricio Avila <p.avil;af1998@gmail.com>
2024-12-25 15:48:13 -0300

    Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavbarComponent from './components/NavbarComponent.vue'
+import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
   <div>
     <NavbarComponent />
+    <HeroSection />
   </div>
 </template>
